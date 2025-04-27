290a535a0e47276fa9a4f1567c177fe929a15388
Ganesh M Mukhmale <145045196+ganeshmukhmale@users.noreply.github.com>
2025-04-27 12:35:30 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Hello{
+    public static void mian (String G []){
+	Scanner sc = new Scanner(System.in);
+	String name = sc.nextLine();
+	System.out.print("Hello Dear: "+name);
+	
+    }
+}
